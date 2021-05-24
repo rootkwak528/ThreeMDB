750a8a0d846ef9bb493d39aea0ca0094952dbc7f
hkjung06 <gnsrb011@gmail.com>
2021-05-24 09:38:23 +0900

    fix: card detail

--- client/src/components/TmdbSearchListItem.vue
+++ client/src/components/TmdbSearchListItem.vue
@@ -1,19 +1,12 @@
 <template>
   <div class="col">
     <div @click="onClickItem" class="card mb-3 item">
-      <div class="row g-0">
-        <div class="col-md-4">
-          <img  
-            v-if="poster_path"
-            :src="poster_path"
-            alt="movie_poster"
-          >
-        </div>
-        <div class="col-md-8">
-          <div class="card-body">
-            <h3 class="card-title">{{ movie.title }}</h3>
-          </div>
-        </div>
+      <div>
+        <img  
+          v-if="poster_path"
+          :src="poster_path"
+          alt="movie_poster"
+        >
       </div>
     </div>
   </div>
@@ -33,7 +26,7 @@ export default {
   computed: {
     poster_path () {
       if (this.movie.poster_path) {
-        return `https://www.themoviedb.org/t/p/w92${this.movie.poster_path}`
+        return `https://www.themoviedb.org/t/p/w185${this.movie.poster_path}`
       } else {
         return ''
       }
--- client/src/views/Tmdb.vue
+++ client/src/views/Tmdb.vue
@@ -34,6 +34,7 @@ export default {
     return {
       selectedMovie: '',
       movieList: ''
+      
     }
   },
   components: {
