46a8b53ee9a1d82ca901c058d0ebea266513fc7d
Root_Kwak <rootkwak528@gmail.com>
2021-05-27 09:36:35 +0900

    build: 백엔드 전용 레포지토리로 구분

--- movies/views.py
+++ movies/views.py
@@ -1,3 +1,4 @@
+from operator import imod
 from .models import Movie
 from .serializers import MovieSerializer
 from community.models import Review, Comment
@@ -10,22 +11,31 @@ from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
 
+import requests
+from decouple import config
+
+
+TMDB_API_KEY = config('TMDB_API_KEY')
+
 
 @api_view(['POST'])
 def movie_create(request):
 
-    db_movie = Movie.objects.filter(movie_id=request.data.get('id'))
+    movie_id = request.data.get('id')
+    movie = Movie.objects.filter(movie_id=movie_id)
 
-    if not db_movie.exists():
-        movie = {
-            'movie_id': request.data.get('id'),
+    if not movie.exists():
+        movie_data = {
+            'movie_id': movie_id,
             'title': request.data.get('title'),
             'overview': request.data.get('overview'),
             'release_date': request.data.get('release_date'),
             'poster_path': request.data.get('poster_path'),
+
+            'youtube_trailer_url': get_trailer_path(movie_id),
         }
         
-        serializer = MovieSerializer(data=movie)
+        serializer = MovieSerializer(data=movie_data)
         if serializer.is_valid(raise_exception=True):
             serializer.save()
             return Response(serializer.data)
@@ -39,10 +49,21 @@ def movie_create(request):
             movie = Movie.objects.prefetch_related(
                 Prefetch('reviews', queryset=reviews),
                 Prefetch('reviews__comments', queryset=comments)
-                ).get(pk=db_movie[0].pk)
+                ).get(pk=movie[0].pk)
 
         except:
             return Response(status=status.HTTP_404_NOT_FOUND)
 
         serializer = MovieSerializer(movie)
         return Response(serializer.data, status=status.HTTP_200_OK)
+
+
+def get_trailer_path(movie_id):
+    # TMDB API request
+    url = f'https://api.themoviedb.org/3/movie/{movie_id}/videos?api_key={TMDB_API_KEY}'
+    response = requests.get(url).json().get('results')
+
+    for res in response:
+        if res.get('type') == 'Trailer':
+            return res.get('key')
+    return ''
