c492efd4afea848d38f6d0eefa3d1c3b6f1370dd
Root_Kwak <rootkwak528@gmail.com>
2021-06-16 17:43:15 +0900

    feat: [signup] 에러 메시지 출력 수정

--- src/views/accounts/Signup.vue
+++ src/views/accounts/Signup.vue
@@ -22,7 +22,7 @@
           @keyup.enter="signup(credentials)">
       </div>
 
-      <button class="btn btn-outline-secondary" @click="signup(credentials)">회원가입</button>
+      <button class="btn btn-outline-secondary mb-3" @click="signup(credentials)">회원가입</button>
 
       <div v-if="errors" class="fade-in">
         <span>{{ errors }}</span>
@@ -50,26 +50,42 @@ export default {
     }
   },
   methods: {
-    signup: function () {
+    signup () {
       this.errors = null
 
-      axios({
-        method: 'post',
-        url: `${SERVER_URL}/accounts/signup/`,
-        data: this.credentials,
-      })
-        .then(res => {
-          console.log(res)
-          this.$router.push({ name: 'Login' })
+      if ( !this.credentials.username ) {
+
+        this.errors = 'ID를 입력해주세요.'
+
+      } else if ( !this.credentials.password ) {
+
+        this.errors = '비밀번호를 입력해주세요.'
+
+      } else if ( !this.credentials.passwordConfirmation ) {
+
+        this.errors = '비밀번호 확인을 입력해주세요.'
+
+      } else {
+        
+        axios({
+          method: 'post',
+          url: `${SERVER_URL}/accounts/signup/`,
+          data: this.credentials,
         })
-        .catch(err => {
-          console.log(err)
-          if (err.response.data.error) {
-            this.errors = err.response.data.error
-          } else if (err.response.data.username) {
-            this.errors = err.response.data.username[0]
-          }
-        })
+          .then(res => {
+            console.log(res)
+            this.$router.push({ name: 'Login' })
+          })
+          .catch(err => {
+            console.log(err)
+            if (err.response.data.error) {
+              this.errors = err.response.data.error
+            } else if (err.response.data.username) {
+              this.errors = err.response.data.username[0]
+            }
+          })
+
+      }
     }
   }
 }
